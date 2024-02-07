763964c8c953eb14e28efb248c526f19c7ed4b36
Skiod <skiod19@gmail.com>
2024-02-07 17:25:26 +0100

    ‘xpush’

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,13 +25,7 @@ export default {
     script : [
       {
         hid: 'clarity-script',
-        innerHTML: `
-          (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "gy942a6ahl");
-        `,
+        innerHTML: "(function(c,l,a,r,i,t,y){ c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}; t=l.createElement(r);t.async=1;t.src='https://www.clarity.ms/tag/'+i; y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, 'clarity', 'script', 'gy942a6ahl');",
       },
       // {
       //   hid: 'brevo-conversations-script',
