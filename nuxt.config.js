144b5155d7ab6c5511d62c227ffdf97ec9dc405b
Skiod <skiod19@gmail.com>
2024-02-11 22:45:15 +0100

    add 404 page

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,5 +71,5 @@ export default {
   router: {
     middleware: 'redirect',
   },
-
+  error: '~/pages/404.vue'
 }
---
+++ pages/404.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="error-page">
+    <h1>404 - Page Not Found</h1>
+    <p>Sorry, the page you are looking for does not exist.</p>
+    <router-link to="/">Go to Home Page</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Error404'
+}
+</script>
+
+<style scoped>
+.error-page {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
